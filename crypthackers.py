7d248dfcdda16d2354a408d0e8318a91cafdc232
AngelSecurityTeam <55148534+AngelSecurityTeam@users.noreply.github.com>
2019-11-10 15:39:56 -0500

    Add files via upload

---
+++ crypthackers.py
@@ -0,0 +1,139 @@
+#https://github.com/AngelSecurityTeam
+import string,os
+from time import sleep
+
+print("""
+\033[1;33m   ____________  ______  ________  _____   ________ __ __________  _____
+  / ____/ __ \ \/ / __ \/_  __/ / / /   | / ____/ //_// ____/ __ \/ ___/
+ / /   / /_/ /\  / /_/ / / / / /_/ / /| |/ /   / ,<  / __/ / /_/ /\__ \ 
+/ /___/ _, _/ / / ____/ / / / __  / ___ / /___/ /| |/ /___/ _, _/___/ / 
+\____/_/ |_| /_/_/     /_/ /_/ /_/_/  |_\____/_/ |_/_____/_/ |_|/____/ 
+\033[1;32m                                                     AngelSecurityTeam
+""")
+
+special = [
+'ł',
+'€',
+'¶',
+'ŧ',
+'←',
+'↓',
+'→',
+'ø',
+'þ',
+'æ',
+'ß',
+'ð',
+'đ',
+'ŋ',
+'ħ',
+'ĸ',
+'ł',
+'~',
+'«',
+'»',
+'¢',
+'“',
+"'",
+'µ',
+'ñ',
+'!',
+'@',
+'$', 
+'%', 
+'^', 
+'&', 
+'*', 
+'(',
+')', 
+'-', 
+'.', 
+'+', 
+'=', 
+'_', 
+',', 
+'|', 
+'?', 
+'>', 
+'<', 
+'/',
+'º',
+'ª',
+'"',
+'¬',
+'~',
+'Ñ',
+'·',
+'Ł',
+'{',
+'}',
+']',
+'m',
+'[',
+" \ ",
+'½',
+'',
+'\\',
+'#']
+numbers = [
+'0',
+'1',
+'2',
+'3',
+'4',
+'5',
+'6',
+'7',
+'8',
+'9']
+
+list = list(string.ascii_letters) + numbers + special 
+
+def encoder():
+    keyinput = input('\033[1;33m\nMESSAGE : \033[1;39m')
+    keyinput2 = eval(input('\033[1;33m\nKEY [1,2,3,4,5,6,7,8,9,0] : \033[1;39m'))
+    sleep(2)
+    code = ''
+    def encoder2(message):
+        j = list.index(message)
+        i = (j+keyinput2)%(len(list) - 1)
+        return i
+    for k in keyinput:
+      if k == ' ':
+         code += ' '
+      else:
+         l = encoder2(k)
+         code += list[l]
+    print('\033[1;32m\n\t\t\t[ENCRYPTING]  ')
+    print("\n"+code)
+    print("\n")
+    optionss()
+
+def decoder():
+    keyinput = input('\033[1;33m\nCODE : \033[1;39m')
+    keyinput2 = eval(input('\033[1;33m\nKEY [1,2,3,4,5,6,7,8,9,0] : \033[1;39m'))
+    sleep(2)
+    message = ''
+    for k in keyinput:
+      if k == ' ':
+         message += ' '
+      else:
+         r = list.index(k)
+         l = (r - keyinput2)%(len(list) - 1)
+         message += list[l]
+    print('\033[1;32m\n\t\t\t[DECRYPTING]  ')
+    print("\n"+message)
+    print("\n")
+    optionss()
+def optionss():
+    print('\033[1;33m1)\033[1;39mENCODING ')
+    print('\n\033[1;33m2)\033[1;39mDECODING  ')
+    print('\n\033[1;33m3)\033[1;39mEXIT')
+    opt= int(input("\033[1;33m\nOPTIONS : "))
+    if opt == 1:
+       encoder()
+    elif opt == 2:
+       decoder()
+    else:
+       exit()
+optionss()
